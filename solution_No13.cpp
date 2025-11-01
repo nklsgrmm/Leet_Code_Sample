1929f10dfe64e7f8095bb49971cd7784c92f3440
nklsgrmm <70199749+nklsgrmm@users.noreply.github.com>
2025-11-01 13:02:31 +0100

    started working on solution_No13

--- solution_No13.cpp
+++ solution_No13.cpp
@@ -1,3 +1,18 @@
 //
 // Created by Niklas Grimm on 01.11.25.
 //
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i =0; i< nums.size(); i++ ){
+            for(int j =i; j< nums.size(); j++ ){
+                if(nums[i]  + nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {0,0};
+    }
+};
+
